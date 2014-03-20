0c3bc23e340ee364f67cb1d10369b49654530846
jdpxiaoming <302940116@qq.com>
2014-03-21 00:15:19 +0800

    init

---
+++ src/com/lewen/listener/activity/ActivitySplash.java
@@ -0,0 +1,18 @@
+package com.lewen.listener.activity;
+
+import com.lewen.listener.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivitySplash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.layout_splash);
+	}
+
+}
