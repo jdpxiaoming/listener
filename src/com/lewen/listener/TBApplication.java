ef1d372dd2164eee56abf808d1e721c6f873517f
jdpxiaoming <302940116@qq.com>
2014-04-04 10:57:35 +0800

    定位到城市

--- src/com/lewen/listener/TBApplication.java
+++ src/com/lewen/listener/TBApplication.java
@@ -1,22 +1,20 @@
 package com.lewen.listener;
 
-import java.io.IOException;
-
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import android.app.AlertDialog;
 import android.app.Application;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.TextView;
 
 /**
  * 一些全局的布置
@@ -30,8 +28,10 @@ public class TBApplication extends Application {
 	public static DisplayMetrics metrics =null;
 	public static ImageLoader imageLoader=ImageLoader.getInstance();
 	// 百度MapAPI的管理类
-	public static	String mStrKey = "D62C7A77394E09E9AA004C7F3B8C589D2F96746D";
-
+	public LocationClient mLocationClient;
+	private MyLocationListener mMyLocationListener;
+	public TextView mLocationResult;
+	
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -52,25 +52,69 @@ public class TBApplication extends Application {
 		}
 		
        
+		
+		mLocationClient = new LocationClient(this);
+		mMyLocationListener = new MyLocationListener();
+		mLocationClient.registerLocationListener(mMyLocationListener);
 	}
 
 	/**
-	 * 提示正在建设中
+	 * 实现实位回调监听
 	 */
-	public static void showBuildTip(Context c){
-		//弹出提示框
-		new AlertDialog.Builder(c)
-					.setTitle("提示")
-					.setMessage("正在建设中...")
-					.setPositiveButton("OK",new DialogInterface.OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.cancel();
-						}
-					}).show();
-	}
+	public class MyLocationListener implements BDLocationListener {
 
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			//Receive Location 
+			StringBuffer sb = new StringBuffer(256);
+//			sb.append("time : ");
+//			sb.append(location.getTime());
+//			sb.append("\nerror code : ");
+//			sb.append(location.getLocType());
+//			sb.append("\nlatitude : ");
+//			sb.append(location.getLatitude());
+//			sb.append("\nlontitude : ");
+//			sb.append(location.getLongitude());
+//			sb.append("\nradius : ");
+//			sb.append(location.getRadius());
+//			if (location.getLocType() == BDLocation.TypeGpsLocation){
+//				sb.append("\nspeed : ");
+//				sb.append(location.getSpeed());
+//				sb.append("\nsatellite : ");
+//				sb.append(location.getSatelliteNumber());
+//				sb.append("\ndirection : ");
+//				sb.append(location.getDirection());
+//			} else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+//				sb.append("\naddr : ");
+//				sb.append(location.getAddrStr());
+//				//运营商信息
+//				sb.append("\noperationers : ");
+//				sb.append(location.getOperators());
+//			}
+			sb.append(location.getCity());
+			logMsg(sb.toString());
+			Log.i("BaiduLocationApiDem", sb.toString());
+		}
+
+		@Override
+		public void onReceivePoi(BDLocation arg0) {
+			
+		}
+	}
+	
+	/**
+	 * 显示请求字符串
+	 * @param str
+	 */
+	public void logMsg(String str) {
+		try {
+			if (mLocationResult != null)
+				mLocationResult.setText(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	/**
 	 * 存放token的方法
