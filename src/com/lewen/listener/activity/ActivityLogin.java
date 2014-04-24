ff750fae9ca99e5e80b9894fde7892bf670d3412
jdpxiaoming <302940116@qq.com>
2014-04-24 15:04:59 +0800

    1.替换帮助页面的图片

--- src/com/lewen/listener/activity/ActivityLogin.java
+++ src/com/lewen/listener/activity/ActivityLogin.java
@@ -2,17 +2,13 @@ package com.lewen.listener.activity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.lewen.listener.R;
 import com.lewen.listener.TBApplication;
 import com.lewen.listener.activity.parent.BaseActivity;
