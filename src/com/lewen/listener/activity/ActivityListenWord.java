c766247556423032df0946e4fdf52218cf3714b9
jdpxiaoming <302940116@qq.com>
2014-04-25 17:40:13 +0800

    17：40

---
+++ src/com/lewen/listener/activity/ActivityListenWord.java
@@ -0,0 +1,55 @@
+package com.lewen.listener.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import com.lewen.listener.R;
+import com.lewen.listener.activity.parent.BaseActivity;
+
+public class ActivityListenWord extends BaseActivity {
+	
+	private TextView mTextViewWord,mTextViewPicture,mTextViewWorld;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.layout_guess_word);
+		
+		init();
+	}
+
+	private void init() {
+		
+		mTextViewWord	=	(TextView) findViewById(R.id.textWord);
+		mTextViewPicture=	(TextView) findViewById(R.id.textPicture);
+		mTextViewWorld	=	(TextView) findViewById(R.id.textWorld);
+		
+		mTextViewWord.setOnClickListener(clickListener);
+		mTextViewPicture.setOnClickListener(clickListener);
+		mTextViewWorld.setOnClickListener(clickListener);
+		
+	}
+
+	OnClickListener clickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+
+			switch (v.getId()) {
+			case R.id.textWord://猜单词
+				
+				break;
+			case R.id.textWorld://听世界
+				
+				break;
+			case R.id.textPicture://猜图片
+				
+				break;
+			default:
+				break;
+			}
+		}
+	};
+}
